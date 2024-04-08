8bed5884db9e7ffcd1d131f8d818fcc1695004bd
Nikolay <mykolagalanix@gmail.com>
2024-04-08 17:45:05 +0300

    install bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,9 +6,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 </head>
 <body>
-    <div>
+<div class="container">
+    <div class="row">
         <nav>
             <ul>
                 <li><a href="{{ route('main.index') }}">Main</a></li>
@@ -20,6 +29,8 @@
     </div>
 
     @yield('content')
+</div>
+
 </body>
 </html>
 
